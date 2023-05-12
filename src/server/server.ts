a0eefa4f0fd8cb0c6e584392615871122fba220e
Fernando Ferreira <f3rnando.ferreira.2023@gmail.com>
2023-05-11 21:36:48 -0300

    feat: adding cors config

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import { router } from '../routes';
-
+import cors from 'cors';
 
 
 const app = express();
+app.use(cors({
+    origin: process.env.ENABLED_CORS?.split(';') || []
+}));
 app.use(express.json());
 app.use(router);
 
